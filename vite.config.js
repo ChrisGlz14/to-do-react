7e4e2909c2be9614bd046da943a8719daf3733b3
Christian Gonzalez <124578802+ChrisGlz14@users.noreply.github.com>
2023-07-24 01:51:40 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://gitlab.com/chrisglz14/to-do-react'
 })
